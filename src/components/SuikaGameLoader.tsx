5206e74c6f3d09838b099dc47c6683447233ccb7
bbielo <eulbi90@gmail.com>
2026-03-12 22:13:38 +0900

    chore: revert file

--- src/components/SuikaGameLoader.tsx
+++ src/components/SuikaGameLoader.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import dynamic from "next/dynamic";
+
+const SuikaGameCanvas = dynamic(
+    () => import("./SuikaGameCanvas"),
+    { ssr: false }
+);
+
 export default function SuikaGameLoader() {
-    return <div>Suika Game Loader</div>;
+    return <SuikaGameCanvas />;
 }
