1f322e3f56a64bcf69cdb70204ef35547d43859e
bbielo <eulbi90@gmail.com>
2026-03-13 16:47:39 +0900

    feat: add ground

--- src/game/engine.ts
+++ src/game/engine.ts
@@ -1,14 +1,17 @@
-import { Engine } from "matter-js";
+import { Engine, Bodies, World } from "matter-js";
 
-// 물리 엔진 생성
 export function createGameEngine() {
+
+    // 물리 엔진 생성
     const engine = Engine.create();
 
-    // x축 중력 - 좌우
+    // 중력 설정
     engine.gravity.x = 0;
-
-    // y축 중력 - 상하
     engine.gravity.y = 1;
+
+    // 바닥설정 (x, y, 너비, 높이, {고정 true})
+    const ground = Bodies.rectangle(200, 610, 400, 20,{ isStatic: true });
+    World.add(engine.world, ground);
 
     return engine;
 }
