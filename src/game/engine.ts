f4f549242c5ad43868f0a7d1e32cffed0c5dcdd3
bbielo <eulbi90@gmail.com>
2026-03-13 17:31:57 +0900

    fix: clean code (engine.ts)

--- src/game/engine.ts
+++ src/game/engine.ts
@@ -2,7 +2,6 @@ import { Bodies, Engine, World } from "matter-js";
 import { GAME_HEIGHT, GAME_WIDTH } from "@/game/constants";
 
 export function createGameEngine() {
-
     // 물리 엔진 생성
     const engine = Engine.create();
 
@@ -10,13 +9,13 @@ export function createGameEngine() {
     engine.gravity.x = 0;
     engine.gravity.y = 1;
 
-    // 바닥설정 (x, y, 너비, 높이, {고정 true})
-    const ground = Bodies.rectangle(GAME_WIDTH / 2, GAME_HEIGHT + 10, GAME_WIDTH, 20, { isStatic: true });
-    
-    // 벽 설정
-    const leftWall = Bodies.rectangle(-10, GAME_HEIGHT / 2, 20, GAME_HEIGHT, { isStatic: true });
-    const rightWall = Bodies.rectangle(GAME_WIDTH -22, GAME_HEIGHT / 2, 20, GAME_HEIGHT, { isStatic: true });
-    
+    // space 설정 : (x, y, 너비, 높이, {고정 true})
+    const wallThickness = 20;
+
+    const ground = Bodies.rectangle(GAME_WIDTH / 2, GAME_HEIGHT + wallThickness / 2, GAME_WIDTH, wallThickness, { isStatic: true } );
+    const leftWall = Bodies.rectangle(0, GAME_HEIGHT / 2, wallThickness, GAME_HEIGHT, { isStatic: true });
+    const rightWall = Bodies.rectangle(GAME_WIDTH, GAME_HEIGHT / 2, wallThickness, GAME_HEIGHT, { isStatic: true } );
+
     World.add(engine.world, [ground, leftWall, rightWall]);
     return engine;
 }
